b4d50265c3ee540cafceeeae995e8ab8ea2fe6fb
Mohammed Arshad <mohammedarshad.arsh@gmail.com>
2023-12-16 00:07:49 +0530

    inital commit after adding redux

--- App.js
+++ App.js
@@ -1,12 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import {Provider} from 'react-redux'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={St}>
+
+      <View
+        style={styles.container}>
+        <Text>O!</Text>
+        <StatusBar
+          style="auto"
+
+        />
+      </View>
+    </Provider>
   );
 }
 
