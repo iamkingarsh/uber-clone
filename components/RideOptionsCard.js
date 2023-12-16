7efeb147d03cc775a602122381848218dc793d03
Mohammed Arshad <mohammedarshad.arsh@gmail.com>
2023-12-16 22:11:18 +0530

    Refactor RideOptionsCard component and add travel time information

--- components/RideOptionsCard.js
+++ components/RideOptionsCard.js
@@ -1,12 +1,21 @@
 import { FlatList, Image, SafeAreaView, ScrollView, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import tw from 'tailwind-react-native-classnames'
 import { Icon } from 'react-native-elements'
 import { useNavigation } from '@react-navigation/native'
+import {  selectTravelTimeInformation } from '../slices/navSlice'
+import { useSelector } from 'react-redux'
 
 const RideOptionsCard = () => {
   const navigation = useNavigation();
   const [selected, setSelected] = useState(null);
+  const travelTimeInformation = useSelector(selectTravelTimeInformation)
+  const SURGE_CHARGE_RATE = 1.5;
+
+  useEffect(() => {
+    console.log(travelTimeInformation)
+  } , [travelTimeInformation])
+
 
   const data = [
     {
@@ -29,6 +38,16 @@ const RideOptionsCard = () => {
     }
   ]
 
+  //convert distance in miles to km
+  const distanceInMiles = travelTimeInformation?.distance?.text?.split(" ")[0];
+  const distanceInMilesRound = Math.round(distanceInMiles * 1.60934);
+  const convertedDistance = distanceInMiles * 1.60934;
+
+  //convert distance in km to 2 decimal places
+  const convertedDistanceRound = convertedDistance.toFixed(2);
+
+  
+
   return (
     <SafeAreaView style={tw`bg-white flex-1 rounded-t-3xl`} >
       <View>
@@ -39,35 +58,47 @@ const RideOptionsCard = () => {
           <Icon name="chevron-left" size={30} type="fontawesome" />
         </TouchableOpacity>
         <Text style={tw`text-center py-5 text-xl font-semibold`}
-        >Select a Ride</Text>
+        >Select a Ride - {convertedDistanceRound} km</Text>
       </View>
 
       <FlatList
         data={data}
         keyExtractor={(item) => item.id}
         renderItem={({ item: { id, title, multiplier, image }, item }) => (
-          <TouchableOpacity style={tw`flex-row mx-4 my-1 rounded-xl justify-between items-center px-10 ${selected?.id === id ? 'bg-gray-200' : 'bg-white'}`}
+          <TouchableOpacity style={tw`flex-row  rounded-xl justify-between items-center px-8 ${selected?.id === id ? 'bg-gray-200' : 'bg-white'}`}
           onPress={() => setSelected(item)}
           >
             <Image
               style={{
-                width: 80,
-                height: 80,
+                width: 100,
+                height: 100,
                 resizeMode: "contain"
               }}
               source={{ uri: image }}
             />
-            <View style={tw`-ml-6`}>
+            <View style={tw`-ml-6 `}>
               <Text style={tw`text-xl font-semibold`}>{title}</Text>
-              <Text>Travel Time...</Text>
+              <Text>{travelTimeInformation?.duration?.text}</Text>
             </View>
-            <Text style={tw`text-xl`}>$99</Text>
+            <Text style={tw`text-xl`}>
+              {
+                new Intl.NumberFormat('en-in', {
+                  style: 'currency',
+                  currency: 'INR'
+                }).format((travelTimeInformation?.distance.value * SURGE_CHARGE_RATE * multiplier) / 100)
+              }
+            </Text>
           </TouchableOpacity>
         )}
         />
         <View>
           <TouchableOpacity disabled={!selected} style={tw`bg-black py-3 m-3 rounded-xl ${!selected && 'bg-gray-300'}`}>
-            <Text style={tw`text-center text-white text-xl`}>Choose {selected?.title}</Text>
+            <Text style={tw`text-center text-white font-bold text-xl`}>Pay  { selected?.multiplier &&
+                new Intl.NumberFormat('en-in', {
+                  style: 'currency',
+                  currency: 'INR'
+                }).format((travelTimeInformation?.distance.value * SURGE_CHARGE_RATE * selected?.multiplier) / 100)
+              }</Text>
           </TouchableOpacity>
         </View>
     </SafeAreaView>
